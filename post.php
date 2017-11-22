05eef9ae2f036dd31f45e91e8be3d93d0cf372f1
Marko <markostevic96@hotmail.com>
2017-11-22 01:55:06 +0100

    easy steps

--- post.php
+++ post.php
@@ -11,10 +11,10 @@
     <title>Blog Post - Start Bootstrap Template</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="bootstrap-post/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="css/blog-post.css" rel="stylesheet">
+    <link href="bootstrap-post/css/blog-post.css" rel="stylesheet">
 
   </head>
 
